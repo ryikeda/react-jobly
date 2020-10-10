6c88da17ad82e50a3719f60245990ecf71b3b671
rafael ubuntu <ryikeda@outlook.com>
2020-10-10 23:57:57 +0900

    added styles to company page

--- src/components/Companies/index.js
+++ src/components/Companies/index.js
@@ -1,7 +1,55 @@
 import React from "react";
+import {
+  Container,
+  TextField,
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+  IconButton,
+  InputAdornment,
+} from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
 
 const Companies = () => {
-  return <div>This is the companies Page</div>;
+  const classes = useStyles();
+  return (
+    <Container>
+      <Box m={3}>
+        <Container maxWidth="sm">
+          <TextField
+            variant="outlined"
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment>
+                  <IconButton>
+                    <SearchIcon />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Container>
+      </Box>
+      <Card>
+        <CardContent>
+          <Typography variant="h6"> Company name</Typography>
+          <Typography variant="body2" component="p">
+            Company description
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </Container>
+  );
 };
 
 export default Companies;
