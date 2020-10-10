9532b22d672be40adbf3507ca6b665be4b739c7e
rafael ubuntu <ryikeda@outlook.com>
2020-10-10 23:57:37 +0900

    added styles to navbar

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,29 +1,63 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import "./style.css";
+import { NavLink } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  ButtonGroup,
+  Button,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+    textAlign: "left",
+  },
+  offset: theme.mixins.toolbar,
+  navLink: {
+    textDecoration: "none",
+    color: "white",
+  },
+}));
 
 const NavBar = () => {
+  const classes = useStyles();
   return (
-    <header>
-      <nav>
-        <NavLink to="/">Jobly</NavLink>
-
-        <ul>
-          <li>
-            <NavLink to="/companies">Companies</NavLink>
-          </li>
-          <li>
-            <NavLink to="/jobs">Jobs</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
-          </li>
-          <li>
-            <NavLink to="/logout">Logout</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <NavLink to="/" className={classes.navLink}>
+              Jobly
+            </NavLink>
+          </Typography>
+          <ButtonGroup>
+            <Button>
+              <NavLink to="/companies" className={classes.navLink}>
+                Companies
+              </NavLink>
+            </Button>
+            <Button>
+              <NavLink to="/jobs" className={classes.navLink}>
+                Jobs
+              </NavLink>
+            </Button>
+            <Button>
+              <NavLink to="/profile" className={classes.navLink}>
+                Profile
+              </NavLink>
+            </Button>
+            <Button>
+              <NavLink to="/logout" className={classes.navLink}>
+                Logout
+              </NavLink>
+            </Button>
+          </ButtonGroup>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.offset}></div>
+    </>
   );
 };
 
